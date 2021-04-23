b88a4742d8a6a3aee81b33ff92c270e23ce50a2f
Ygor Azambuja <ygorazambuja@gmail.com>
2021-04-23 10:19:52 -0400

    fix: retirando borda da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -25,7 +25,6 @@ export default class App extends Component {
           <Box
             align="center"
             pad="medium"
-            border={{ color: 'brand', size: 'small' }}
             round
           >
             <Heading size="large">{'{SCDE}'}</Heading>
